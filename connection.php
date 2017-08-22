36377a3bdfa569d6b3af509806793657a0aaa06c
BonaniPaul <31246858+BonaniPaul@users.noreply.github.com>
2017-08-22 20:25:46 +0530

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,19 @@
+<?php
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "root";
+$dbname = "Employee";
+
+// Create connection
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+    echo ' Connection not established ';
+}
+
+	echo "connected successfully";
+
+
+$con->close();
+?>
